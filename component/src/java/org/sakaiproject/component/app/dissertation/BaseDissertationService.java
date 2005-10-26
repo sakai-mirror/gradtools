2544460b75ce7eeda945dd99bcad077ed3403c69
ggolden@umich.edu <ggolden@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2005-10-26 03:32:16 +0000

    SAK-2122 - site change notification to EntityProducers, cleanup of the Site API component
    git-svn-id: https://source.sakaiproject.org/svn/trunk/ctools/gradtools@2965 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- component/src/java/org/sakaiproject/component/app/dissertation/BaseDissertationService.java
+++ component/src/java/org/sakaiproject/component/app/dissertation/BaseDissertationService.java
@@ -74,6 +74,7 @@ import org.sakaiproject.service.framework.session.UsageSession;
 import org.sakaiproject.service.legacy.entity.Edit;
 import org.sakaiproject.service.legacy.entity.Entity;
 import org.sakaiproject.service.legacy.entity.EntityManager;
+import org.sakaiproject.service.legacy.entity.EntityProducer;
 import org.sakaiproject.service.legacy.entity.Reference;
 import org.sakaiproject.service.legacy.entity.ResourceProperties;
 import org.sakaiproject.service.legacy.entity.ResourcePropertiesEdit;
@@ -81,6 +82,7 @@ import org.sakaiproject.service.legacy.event.Event;
 import org.sakaiproject.service.legacy.event.cover.EventTrackingService;
 import org.sakaiproject.service.legacy.id.cover.IdService;
 import org.sakaiproject.service.legacy.security.cover.SecurityService;
+import org.sakaiproject.service.legacy.site.Site;
 import org.sakaiproject.service.legacy.time.Time;
 import org.sakaiproject.service.legacy.time.TimeBreakdown;
 import org.sakaiproject.service.legacy.time.cover.TimeService;
@@ -6816,6 +6818,13 @@ public abstract class BaseDissertationService
 	public String getEntityUrl(Reference ref)
 	{
 		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void syncWithSiteChange(Site site, EntityProducer.ChangeType change)
+	{
 	}
 
 	/**
